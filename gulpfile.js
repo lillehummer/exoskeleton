177b452fac59427b5645dd4c8bb73998e00831b5
Charles Beaumont <charles@lillehummer-macbook.local>
2017-05-05 22:12:07 +0200

    Hello again, Webpack!

--- gulpfile.js
+++ gulpfile.js
@@ -2,27 +2,12 @@
 var gulp = require('gulp');
 
 // Include plugins
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var filter = require('gulp-filter');
-var mainBowerFiles = require('main-bower-files');
-var browserSync = require('browser-sync');
 var gulpUtil = require('gulp-util');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var changed = require('gulp-changed');
-var mqpacker = require('css-mqpacker');
 var plumber = require('gulp-plumber');
-var sourcemaps = require('gulp-sourcemaps');
-var revAll = require('gulp-rev-all');
-var revDel = require('rev-del');
 var notify = require('gulp-notify');
 var exec = require('child_process').exec;
-var webpack = require('webpack');
-var webpackStream = require('webpack-stream');
 
 // Error handler
 var onError = function(err) {
@@ -35,39 +20,6 @@ var onError = function(err) {
     gulpUtil.log(gulpUtil.colors.red(err));
     this.emit('end');
 };
-
-//
-// DEFAULT
-//
-
-    gulp.task('default', ['browser-sync'], function(){
-
-        gulp.watch(['src/js/*.js', 'src/js/**/*.js'], ['js']).on('change', browserSync.reload);
-        gulp.watch('src/rev/functions.php', ['rev']);
-        gulp.watch(['src/css/*.scss', 'src/css/**/*.scss'], ['css']);
-        gulp.watch(['src/img/*'], ['images']);
-
-    });
-
-//
-// BROWSER SYNC
-//
-
-    gulp.task('browser-sync', function() {
-
-        var files = [
-        './css/style.css',
-        './*.php',
-        './**/*.php',
-        './**/**/*.php'
-        ];
-
-        browserSync.init(files, {
-            proxy: 'lillehummernl.dev',
-            notify: false
-        });
-
-    });
 
 //
 // OPTIMIZE IMAGES
@@ -92,145 +44,6 @@ var onError = function(err) {
                 use: [pngquant()]
             }))
             .pipe(gulp.dest('img'))
-            .pipe(notify(notification));
-
-    });
-
-//
-// BUILD CSS
-//
-
-    gulp.task('css', function(callback) {
-
-        var notification = {
-            title: 'Task',
-            message: 'css finished',
-            onLast: true
-        };
-
-        var processors = [
-            autoprefixer({browsers: ['last 2 version', 'ie 10']}),
-            mqpacker
-        ];
-
-        return gulp.src('src/css/*.scss')
-            .pipe(plumber({errorHandler: onError}))
-            .pipe(sourcemaps.init())
-            .pipe(sass())
-            .pipe(postcss(processors))
-            .pipe(sourcemaps.write())
-            .pipe(gulp.dest('css'))
-            .pipe(browserSync.stream())
-            .pipe(notify(notification));
-
-    });
-
-//
-// BUILD JAVASCRIPT
-//
-
-    gulp.task('js', function(callback) {
-
-        var notification = {
-            title: 'Task',
-            message: 'js finished',
-            onLast: true
-        };
-
-        return gulp.src('src/js/app.js')
-            .pipe(plumber({errorHandler: onError}))
-            .pipe(sourcemaps.init())
-            .pipe(webpackStream(require('./webpack.config.js'), webpack ))
-            .pipe(sourcemaps.write())
-            .pipe(gulp.dest('js'))
-            .pipe(notify(notification));
-
-    });
-
-//
-// CREATE REVISIONS
-//
-
-    gulp.task('rev', function(callback) {
-
-        var notification = {
-            title: 'Task',
-            message: 'rev finished',
-            onLast: true
-        };
-
-        var revOptions = {
-            dontRenameFile: ['functions.php']
-        };
-
-        return gulp.src(['src/rev/**'])
-                .pipe(plumber({errorHandler: onError}))
-                .pipe(revAll.revision(revOptions))
-                .pipe(gulp.dest('./'))
-                .pipe(revAll.manifestFile())
-                .pipe(revDel('src/rev-manifest.json'))
-                .pipe(gulp.dest('src'))
-                .pipe(notify(notification));
-
-    });
-
-//
-// SHELL COMMANDS
-//
-
-    gulp.task('push', function (cb) {
-      exec('wordmove push -e staging -t', function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
-        cb(err);
-      });
-    });
-
-    gulp.task('pull', function (cb) {
-      exec('wordmove pull -e staging -du', function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
-        cb(err);
-      });
-    });
-
-    gulp.task('bower', function (cb) {
-      exec('bower update', function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
-        cb(err);
-      });
-    });
-
-//
-// DO I USE
-//
-
-    gulp.task('doiuse', function(callback) {
-
-        var notification = {
-            title: 'Task',
-            message: 'doiuse finished',
-            onLast: true
-        };
-
-        var processors = [
-            doiuse({
-                browsers: [
-                  'ie >= 11',
-                  '> 3% in NL'
-                ],
-                ignore: [],
-                ignoreFiles: ['**/normalize.css'],
-                onFeatureUsage: function (usageInfo) {
-                  console.log(usageInfo.message)
-                }
-            })
-        ];
-
-        return gulp.src('src/css/*.scss')
-            .pipe(sass({ outputStyle: 'compressed' }))
-            .pipe(postcss(processors))
             .pipe(notify(notification));
 
     });
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,38 @@
+// TODO:
+// - Vendor bundling
+// - OSX notifications
+// - Hot module reloading
+// - Friendly errors
+// - Performance
+// - Image optimisation
+// - Prod/Dev plugins
+// - More PostCSS
+
 var webpack = require('webpack');
 var path = require('path');
 
-module.exports =  {
+require('dotenv').config({path: __dirname + '/../../../.env'})
+
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+var ManifestPlugin = require('webpack-manifest-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+var env = process.env.NODE_ENV || 'development';
+var isProd = (env === 'production');
+
+var config =  {
+  entry: {
+    app: ['./src/js/app.js', './src/css/style.scss']
+  },
   output: {
-    filename: 'app.js'
+    filename: isProd ? './js/[name].[hash].js' : './js/[name].js'
   },
   module: {
     loaders: [
       {
         test: /\.js$/,
-        exclude: [/(node_modules|bower_components)(?![/|\\](bootstrap|foundation-sites))/],
+        exclude: [/(node_modules|bower)/],
         use: [{
           loader: 'buble-loader',
           options: { objectAssign: 'Object.assign' },
@@ -18,14 +41,63 @@ module.exports =  {
       {
         test: /\.vue$/,
         loader: 'vue-loader'
-      }
+      },
+      {
+          test: /\.(scss)$/,
+          use: ExtractTextPlugin.extract([
+            {
+              loader: 'css-loader',
+              options: {
+                url: false,
+                minimize: isProd ? true : false
+              }
+            },
+            'postcss-loader',
+            {
+              loader: "sass-loader", options: {
+              }
+            }
+          ])
+        }
     ]
   },
+  plugins: [
+    new ManifestPlugin(),
+    new ExtractTextPlugin( isProd ? './css/style.[hash].css' : './css/style.css' ),
+    new BrowserSyncPlugin({
+      host: 'localhost',
+      port: 3000,
+      proxy: process.env.PROXY,
+      notify: false,
+      files: ['./css/style.css', './*.php', './**/*.php', './**/**/*.php']
+    })
+  ],
   resolve: {
     modules: ['node_modules']
   },
   performance: {
     maxAssetSize: 2500000,
     maxEntrypointSize: 2500000
+  },
+  devServer: {
+    headers: {
+        "Access-Control-Allow-Origin": "*"
+    },
+    historyApiFallback: true,
+    noInfo: true,
+    compress: true,
+    quiet: true
   }
 };
+
+if ( isProd ) {
+    config.plugins.push(
+        new UglifyJSPlugin({
+          mangle: {
+            except: ['$', 'exports', 'require', 'import']
+          }
+        })
+    );
+}
+
+module.exports = config;
